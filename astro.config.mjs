c70967c51fdcf726ce9dbe89dab87341d4ad9fa2
1700563146 <1700563146@qq.com>
2026-06-22 21:11:10 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://your-domain.com',
+  // 改成你的 GitHub Pages 地址
+  site: 'https://1700563146.github.io',
+  // 添加 base，指向你的仓库名
+  base: '/Satone-blog',
   output: 'static',
   trailingSlash: 'always',
   integrations: [sitemap()],
